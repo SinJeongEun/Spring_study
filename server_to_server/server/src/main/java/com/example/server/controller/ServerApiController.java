01565c68a419b7b7eae2bd1493a1edf0cf0397bd
SinJeongEun <49307620+SinJeongEun@users.noreply.github.com>
2022-04-10 01:06:02 +0900

    네이버 API 연동 실습 -controller

--- server_to_server/server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server_to_server/server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -3,8 +3,15 @@ package com.example.server.controller;
 import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+
 
 @Slf4j
 @RestController
@@ -18,6 +25,36 @@ public class ServerApiController {
         user.setAge(age);
         return user;
     }
+
+    //https://openapi.naver.com/v1/search/local.json?query=%EC%A3%BC%EC%8B%9D&display=10&start=1&sort=random
+    @GetMapping("/naver")
+    public String naver(){
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("v1/search/local.json")
+                .queryParam("query","갈비집")
+                .queryParam("displqy",10)
+                .queryParam("start",1)
+                .queryParam("sort","random")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
+
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> requestEntitye = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id","uZ9bN_o47cRIM0LrP_qD")
+                .header("X-Naver-Client-Secret","zklQDwlTZZ")
+                .build();
+
+        ResponseEntity<String> response = restTemplate.exchange(requestEntitye,String.class);
+
+        return response.getBody();
+    }
+
 
     @PostMapping("/user/{userId}/name/{username}")
     public Req<User> post(@RequestBody Req<User> user,
