2fde1d69156a80b528250463f97d551cf4eeee21
SinJeongEun <49307620+SinJeongEun@users.noreply.github.com>
2022-06-04 11:03:15 +0900

    matcher,paging

--- bookmanager/src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
+++ bookmanager/src/test/java/com/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -5,10 +5,12 @@ import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.endsWith;
 
 
 @SpringBootTest
@@ -39,5 +41,39 @@ class UserRepositoryTest {
     void findByID(){
         User user = userRepository.findById(1L).orElse(null); //findById는 optional 객체로 매핑된 객체를 return 해줌으로, 이와 같은 처리가 필요하다.
         System.out.println(user);
+    }
+
+    @Test
+    void deleteInBatch(){
+        //deleteAll()은 for loop를 통해 각각을 삭제하기 때문에, 성능이슈가 발생됨,,, 또한 delete 이전에 select 로 각 entity가 존재하는지 확인한다.
+        //이를 해결하는 방법이 deleteAllInBatch 이다.
+        //entity가 있던지 없던지 쿼리를 바로 던진다.(delete 이전에 select 하지 않는다.)
+
+//        userRepository.deleteAll();
+        userRepository.deleteAllInBatch();
+        userRepository.findAll().forEach(System.out::println);
+    }
+
+    @Test
+    void paging(){
+        Page<User> users = userRepository.findAll(PageRequest.of(1,3)); // 한페이지에 3개씩 처리
+
+        System.out.println("page : " + users);
+        System.out.println("total elements : " + users.getTotalElements());
+        System.out.println("total pages : " + users.getTotalPages());
+        System.out.println("numberOfElements : "+ users.getNumberOfElements());
+        System.out.println("size : " + users.getSize());
+    }
+    
+    //query by example : entity를 example로 만들고, matcher를 선언하여, 필요한 쿼리들을 만드는 방법이다.
+    @Test
+    void qbe(){
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withIgnorePaths("name") // 무시할 속성
+                .withMatcher("email", endsWith());
+
+        Example<User> example = Example.of(new User("ma","nate.com"),matcher); // 이 객체는 probe로 실제 entityㄱ ㅏ아니다.
+        //email 이 nate.come 으로 끝나느 항목을 찾는다.
+        userRepository.findAll(example).forEach(System.out::println);
     }
 }
