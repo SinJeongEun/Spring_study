90c17fce64933b1163a6a677f9b77712783a3fbb
SinJeongEun <49307620+SinJeongEun@users.noreply.github.com>
2022-06-23 02:51:19 +0900

    entity 생명주기

---
+++ bookmanager/src/main/java/com/jpa/bookmanager/service/UserService.java
@@ -0,0 +1,30 @@
+package com.jpa.bookmanager.service;
+
+import com.jpa.bookmanager.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@Service
+public class UserService {
+    @Autowired
+    private EntityManager entityManager;
+
+    @Transactional
+    public void put(){
+        User user = new User();
+        user.setName("newUser");
+        user.setEmail("newUser@naver.com");
+
+        entityManager.persist(user); // save()와 같다. save() 함수 안에 이코드가 내제되어있다.
+        entityManager.detach(user);
+
+        user.setName("newUserAfterPersist");
+        entityManager.merge(user);
+
+        entityManager.clear();
+
+    }
+}
